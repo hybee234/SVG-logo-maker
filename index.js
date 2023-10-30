5c4a2f5f238791c56be58135e73fa98735198eac
Hybee <hybee234@outlook.com>
2023-10-31 02:50:25 +1100

    prototype solution created

--- index.js
+++ index.js
@@ -11,66 +11,82 @@ const circle = require ('./lib/Circle.js');
 //--------------------//
 
 //https://www.w3.org/TR/SVG11/types.html#ColorKeywords
-const SVGColor = ["alice blue", "antiquewhite", "red", "blue", "green"]
-
-
-var initialise = () => {
-    console.log("Hello world")
-
-
-// #8F5DBA = purple
-// #FFA000 = Orange
-// #0AC600 = Green
-// #0024C6 = Blue
-// #DA0000 = Red
-// #EAF104 = Yellow
+//const SVGColor = ["alice blue", "antiquewhite", "red", "blue", "green"]
 
 //---------------------------//
 //- Prompts to the end user =//
 //---------------------------//
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'userShape',
-            message: 'Shape: What shape would you like your logo?',
-            choices: [
-                {name: 'Circle'},
-                {name: 'Square'},
-                {name: 'Triangle'},
-            ],
-        },
-        {
-            type: 'input',
-            name: 'userShapeColour',
-            message: 'Shape - What colour do you want your shape?',            
-        },
-        //hexadecimal
-        //keyword
-        {
-            type: 'input',
-            name: 'userText',
-            message: 'Text - What text do you want to appear? (3 characters max)',
-        //need to build logic on characters acceptable (regex check, 3 letters)
-        },
-        {
-            type: 'input',
-            name: 'userTextColour',
-            message: 'What colour do you want your text?',
-        },      
-        //hexadecimal
-        //keyword
-    ])
+// function questions () {
+    
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'userShape',
+                message: 'Shape: What shape would you like your logo?',
+                choices: [
+                    {name: 'Circle', value: 'circle'},
+                    {name: 'Square', value: 'square'},
+                    {name: 'Triangle', value: 'triangle'},
+                ],
+            },
+            {
+                type: 'input',
+                name: 'userShapeColour',
+                message: 'Shape - What colour do you want your shape?',            
+            },
+            {
+                type: 'input',
+                name: 'userText',
+                message: 'Text - What text do you want to appear? (3 characters max)',
+            },
+            {
+                type: 'input',
+                name: 'userTextColour',
+                message: 'What colour do you want your text?',
+            },      
+        ])
 
-    .then (answers) = {
-        // switch (answers.userShape) {
-        //     case "Circle":
+        .then ((answers) => {
+            console.log(answers)
+        
+            
+            // switch (answers.userShape) {
+            //     case "Circle":
+            renderFile(answers);
+        })
+            // }
 
-        // }
-    };
+        
+// }
 
-SVG = circle.renderSVG("#EAF104", "123", "#DA0000");
+function renderFile(answers) {
+    console.log("Hello world")
+
+    // #8F5DBA = purple
+    // #FFA000 = Orange
+    // #0AC600 = Green
+    // #0024C6 = Blue
+    // #DA0000 = Red
+    // #EAF104 = Yellow
+console.log(answers.userShape)
+console.log(answers.userShapeColour)
+console.log(answers.userText)
+console.log(answers.userTextColour)
+
+if (answers.userShape === 'circle') {
+    SVG = circle.renderSVG(answers.userShapeColour, answers.userText, answers.userTextColour)
+}
+if (answers.userShape === 'rectangle') {
+    SVG = rectangle.renderSVG(answers.userShapeColour, answers.userText, answers.userTextColour)
+}
+if (answers.userShape === 'triangle') {
+    SVG = triangle.renderSVG(answers.userShapeColour, answers.userText, answers.userTextColour)
+}
+
+
+//SVG = circle.renderSVG("#EAF104", "123", "#0AC600");
 //replace circle with variable that handles rectangle, triangle, circle
 //replace values with responses - userShapeColour, userText, userTextColour
 
@@ -78,7 +94,6 @@ SVG = circle.renderSVG("#EAF104", "123", "#DA0000");
 
 };
 
-initialise();
 
 
 
