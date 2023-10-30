93674faa9e6c88d5887699869af8a2a731d3b71b
Hybee <hybee234@outlook.com>
2023-10-31 02:15:23 +1100

    building inquirer, modularised Circle class and shape class

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 //- Required packages -// 
 //---------------------//
 const fs = require('fs');
+//const path = require('path');
 const inquirer = require('inquirer');
+const circle = require ('./lib/Circle.js');
 
 //--------------------//
 //- Global Variables -//
@@ -11,65 +13,9 @@ const inquirer = require('inquirer');
 //https://www.w3.org/TR/SVG11/types.html#ColorKeywords
 const SVGColor = ["alice blue", "antiquewhite", "red", "blue", "green"]
 
-//---------------------------//
-//- Prompts to the end user =//
-//---------------------------//
-
-var questions = () => {
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'shape',
-            message: 'Shape: What shape would you like your logo?',
-        },
-
-
-        {
-            type: 'input',
-            name: 'shapeColour',
-            message: 'What colour do you want your shape?',
-        },
-        //hexadecimal
-        //keyword
-        {
-            type: 'input',
-            name: 'text',
-            message: 'What text do you want to appear? (3 characters max)',
-
-        //need to build logic on characters acceptable (regex check, 3 letters)
-
-        },
-        {
-            type: 'input',
-            name: 'textColour',
-            message: 'What colour do you want your text?',
-        },      
-        //hexadecimal
-        //keyword
-    ])
-}
 
 var initialise = () => {
     console.log("Hello world")
-
-    class Circle {
-        constructor (shapeColour, text, textColour) {
-        this.shapeColour = shapeColour;
-        this.text = text
-        this.textColour = textColour
-
-        }    
-    }
-
-    const newLogo = new Circle ("#8F5DBA", "ABC", "#EAF104")
-
-    const circleSVG = `
-<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">    
-    <circle cx="150" cy="150" r="100" fill="${newLogo.shapeColour}" />
-    <text x="150" y="170" font-size="70" text-anchor="middle" fill="${newLogo.textColour}"> ${newLogo.text} </text>
-    Sorry, your browser does not support inline SVG.
-</svg>`
 
 
 // #8F5DBA = purple
@@ -79,12 +25,58 @@ var initialise = () => {
 // #DA0000 = Red
 // #EAF104 = Yellow
 
+//---------------------------//
+//- Prompts to the end user =//
+//---------------------------//
 
-    fs.writeFile ('./output/logo.svg/', circleSVG, (err) => err ? console.log (err) : console.log ('SVG File created'));
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'userShape',
+            message: 'Shape: What shape would you like your logo?',
+            choices: [
+                {name: 'Circle'},
+                {name: 'Square'},
+                {name: 'Triangle'},
+            ],
+        },
+        {
+            type: 'input',
+            name: 'userShapeColour',
+            message: 'Shape - What colour do you want your shape?',            
+        },
+        //hexadecimal
+        //keyword
+        {
+            type: 'input',
+            name: 'userText',
+            message: 'Text - What text do you want to appear? (3 characters max)',
+        //need to build logic on characters acceptable (regex check, 3 letters)
+        },
+        {
+            type: 'input',
+            name: 'userTextColour',
+            message: 'What colour do you want your text?',
+        },      
+        //hexadecimal
+        //keyword
+    ])
+
+    .then (answers) = {
+        // switch (answers.userShape) {
+        //     case "Circle":
+
+        // }
+    };
+
+SVG = circle.renderSVG("#EAF104", "123", "#DA0000");
+//replace circle with variable that handles rectangle, triangle, circle
+//replace values with responses - userShapeColour, userText, userTextColour
+
+    fs.writeFile ('./output/logo.svg/', SVG, (err) => err ? console.log (err) : console.log ('SVG File created'));
 
 };
-
-
 
 initialise();
 
