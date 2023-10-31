8d0d1fbdc66cfc0673160046cff2fdfeb82e1b9d
Hybee <hybee234@outlook.com>
2023-10-31 14:04:59 +1100

    Inquirer validation in place. Square, Triangle and Circle ready, SVG output online.

--- index.js
+++ index.js
@@ -2,18 +2,17 @@
 //- Required packages -// 
 //---------------------//
 const fs = require('fs');
-//const path = require('path');
 const inquirer = require('inquirer');
 const circle = require ('./lib/Circle.js');
 const square = require ('./lib/Square.js');
 const triangle = require ('./lib/Triangle.js');
+const questions = require ('./lib/questions.js')
 
 //--------------------//
 //- Global Variables -//
 //--------------------//
 
-//https://www.w3.org/TR/SVG11/types.html#ColorKeywords
-//const SVGColor = ["alice blue", "antiquewhite", "red", "blue", "green"]
+
 
 //---------------------------//
 //- Prompts to the end user =//
@@ -22,49 +21,20 @@ const triangle = require ('./lib/Triangle.js');
 // function questions () {
     
     inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'userShape',
-                message: 'Shape: What shape would you like your logo?',
-                choices: [
-                    {name: 'Circle', value: 'circle'},
-                    {name: 'Square', value: 'square'},
-                    {name: 'Triangle', value: 'triangle'},
-                ],
-            },
-            {
-                type: 'input',
-                name: 'userShapeColour',
-                message: 'Shape - What colour do you want your shape?',            
-            },
-            {
-                type: 'input',
-                name: 'userText',
-                message: 'Text - What text do you want to appear? (3 characters max)',
-            },
-            {
-                type: 'input',
-                name: 'userTextColour',
-                message: 'What colour do you want your text?',
-            },      
-        ])
+        .prompt(questions)
 
         .then ((answers) => {
-            console.log(answers)
-        
-            
-            // switch (answers.userShape) {
-            //     case "Circle":
+            console.log(answers)   
             renderFile(answers);
         })
-            // }
 
+        .catch ((err) => {
+            console.log(err)
+        });
         
 // }
 
 function renderFile(answers) {
-    console.log("Hello world")
 
     // #8F5DBA = purple
     // #FFA000 = Orange
@@ -72,10 +42,6 @@ function renderFile(answers) {
     // #0024C6 = Blue
     // #DA0000 = Red
     // #EAF104 = Yellow
-console.log(answers.userShape)
-console.log(answers.userShapeColour)
-console.log(answers.userText)
-console.log(answers.userTextColour)
 
 if (answers.userShape === 'circle') {
     SVG = circle.renderSVG(answers.userShapeColour, answers.userText, answers.userTextColour)
@@ -92,7 +58,7 @@ if (answers.userShape === 'triangle') {
 //replace circle with variable that handles rectangle, triangle, circle
 //replace values with responses - userShapeColour, userText, userTextColour
 
-    fs.writeFile ('./output/logo.svg/', SVG, (err) => err ? console.log (err) : console.log ('SVG File created'));
+    fs.writeFile ('./output/logo.svg/', SVG, (err) => err ? console.log (err) : console.log ('SVG File created http://127.0.0.1:5501/Challenges/SVG-logo-maker/output/logo.svg' ));
 
 };
 
